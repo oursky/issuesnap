3f508c6c0f7bc82e6acfb7320a5227376b7ed568
joyzoursky <joyz@oursky.com>
2024-07-15 19:44:24 +0800

    UI Revamp (#14)
    * Design update following given Figma

--- src/bug_report_generator.py
+++ src/bug_report_generator.py
@@ -2,63 +2,68 @@ import logging
 import os
 from dotenv import load_dotenv
 from langchain_google_genai import (
-    ChatGoogleGenerativeAI,
-    HarmBlockThreshold,
-    HarmCategory,
+	ChatGoogleGenerativeAI,
+	HarmBlockThreshold,
+	HarmCategory,
 )
 from closest_sample_finder import find_closest_samples
 
 # Configure logging (optional, adjust log level and output as needed)
-logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+logging.basicConfig(level=logging.INFO,
+					format='%(asctime)s - %(levelname)s - %(message)s')
+
 
 def check_api_key():
-    load_dotenv()
-    google_api_key = os.getenv("GOOGLE_API_KEY")
-    if not google_api_key:
-        raise KeyError("GOOGLE_API_KEY")
+	load_dotenv()
+	google_api_key = os.getenv("GOOGLE_API_KEY")
+	if not google_api_key:
+		raise KeyError("GOOGLE_API_KEY")
+
 
 def init_llm():
-    try:
-        load_dotenv()
-        google_api_key = os.getenv("GOOGLE_API_KEY")
-        # Initialize LLM model
-        llm = ChatGoogleGenerativeAI(
-            model="gemini-pro",
-            safety_settings={
-                HarmCategory.HARM_CATEGORY_HARASSMENT: HarmBlockThreshold.BLOCK_NONE,
-                HarmCategory.HARM_CATEGORY_HATE_SPEECH: HarmBlockThreshold.BLOCK_NONE,
-                HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT: HarmBlockThreshold.BLOCK_NONE,
-                HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT: HarmBlockThreshold.BLOCK_NONE,
-            },
-        )
-        return llm
-    except Exception as e:
-        logging.error(f"bug_report_generator.py: Error initializing LLM: {e}")
-        raise
+	try:
+		load_dotenv()
+		google_api_key = os.getenv("GOOGLE_API_KEY")
+		# Initialize LLM model
+		llm = ChatGoogleGenerativeAI(
+			model="gemini-pro",
+			safety_settings={
+				HarmCategory.HARM_CATEGORY_HARASSMENT: HarmBlockThreshold.BLOCK_NONE,
+				HarmCategory.HARM_CATEGORY_HATE_SPEECH: HarmBlockThreshold.BLOCK_NONE,
+				HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT: HarmBlockThreshold.BLOCK_NONE,
+				HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT: HarmBlockThreshold.BLOCK_NONE,
+			},
+		)
+		return llm
+	except Exception as e:
+		logging.error(f"bug_report_generator.py: Error initializing LLM: {e}")
+		raise
+
 
 def split_response(content):
-    try:
-        content_split = content.split("Issue Title:")[1].split("Issue Body:")
-        title = content_split[0].strip("*").strip("`").strip()
-        body = content_split[1].strip("*").strip("`").strip()
-        return title, body
-    except Exception as e:
-        logging.error(f"bug_report_generator.py: Error parsing response: {e}")
+	try:
+		content_split = content.split("Issue Title:")[1].split("Issue Body:")
+		title = content_split[0].strip("*").strip("`").strip()
+		body = content_split[1].strip("*").strip("`").strip()
+		return title, body
+	except Exception as e:
+		logging.error(f"bug_report_generator.py: Error parsing response: {e}")
 
-def process_user_input(user_steps, expected_results):
 
-    llm = init_llm()
-    samples = []
+def process_user_input(user_steps, expected_results, test_env):
 
-    try:
-        samples = find_closest_samples(user_steps, expected_results)
-    except Exception as e:
-        logging.error(f"bug_report_generator.py: Error finding closest samples: {e}")
+	llm = init_llm()
+	samples = []
 
-    try:
-        # Send custom prompt if similar issue is found
-        if len(samples) > 0:
-            prompt = """You are a software QA tester. The user reported encountering a bug.
+	try:
+		samples = find_closest_samples(user_steps, expected_results)
+	except Exception as e:
+		logging.error(
+			f"bug_report_generator.py: Error finding closest samples: {e}")
+
+	try:
+		# Send custom prompt
+		prompt = """You are a software QA tester. The user reported encountering a bug.
 Steps to reproduce:
 {user_steps}
 Expected Results:
@@ -70,47 +75,25 @@ Use simple and short sentences. Use similar format and punctuations.
 Below are some examples of bug report:
 """.format(user_steps=user_steps, expected_results=expected_results)
 
-            for sample in samples:
-                prompt += f"{sample}"
+		for sample in samples:
+			prompt += f"{sample}"
 
-            # Generate custom bug report using LLM
-            response = llm.invoke(prompt)
-            title, body = split_response(response.content)
-            
-            report = f"\n*Cool Tip💡 If the result is not accurate, try to refine your input with more information.*\n"
-            report += f"\n**Issue Title:**\n```\n"
-            report += title
-            report += f"\n```\n"
-            report += f"\n**Issue Body:**\n```\n"
-            report += body
-            report += f"\n```\n"
-            report += f"\nDon’t forget to include your test environment details, as well as any screenshots or screen recordings, when submitting your issue.🚀\n"
-            report += """
-Below is a template for test environment information:
-```
-## Environment:
-- Environment: UAT
-- Platform: 
-- Build version: 
-- Browser: 
-- Device (OS version): 
-- Test account: 
-```
-  """
-            report += f"\nHappy Testing!🌟"
-            return report
-        else:
-            # Send a general prompt if no similar issue is found
-            prompt = f"The user reported encountering a bug. Steps to reproduce:\n{user_steps}\nExpected Results:\n{expected_results}\n"
-            prompt += "Can you generate a detailed bug report describing the issue and potential solutions?"
+		# Generate custom bug report using LLM
+		response = llm.invoke(prompt)
+		title, body = split_response(response.content)
 
-             # Generate general bug report using LLM
-            response = llm.invoke(prompt)
-            
-            report = f"\n*Cool Tip💡 If the result is not accurate, try to refine your input with more information.*\n\n"
-            report += response.content
-
-            return report
-    except Exception as e:
-        logging.error(f"bug_report_generator.py: Error generating bug report: {e}")
-        raise
+		report = f"\nIssue Title\n```\n"
+		report += title
+		report += f"\n```\n"
+		report += f"\nIssue Body\n```\n"
+		report += body
+		report += f"\n\n## Environment:\n"
+		report += test_env
+		report += f"\n```\n"
+		report += f"📎 Attachments\n\n"
+		report += f"Don’t forget to include any **screenshots** 📲 or **screen recordings** 🎥 when submitting your issue!"
+		return report
+	except Exception as e:
+		logging.error(
+			f"bug_report_generator.py: Error generating bug report: {e}")
+		raise
--- src/chatbot.py
+++ src/chatbot.py
@@ -1,85 +1,175 @@
 import logging
-import streamlit as st
+import streamlit as st  # type: ignore
 from bug_report_generator import check_api_key, process_user_input
 
 # Configure logging (optional, adjust log level and output as needed)
-logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+logging.basicConfig(level=logging.INFO,
+					format='%(asctime)s - %(levelname)s - %(message)s')
+
 
 def get_user_steps():
-    return st.text_area("**How do you reproduce the bug?**",
-                        placeholder="e.g.\n1. Click Login button\n2. Fill in email and OTP\n3. Click Submit button\n4. See an error \"Failed to login\"",
-                        value="",
-                        height=150)
+	return st.text_area("How do you reproduce the bug? 🐞",
+						placeholder="e.g.\nClick login, fill email & OTP,  click submit, \"Failed to Login\"",
+						value="",
+						height=150)
 
 
 def get_expected_results():
-    return st.text_area("**What do you expect to see?**",
-                        placeholder="e.g. Should redirect to Home page.",
-                        value="",
-                        height=100)
+	return st.text_area("What do you expect to see? 👀",
+						placeholder="e.g.\nRedirect to homepage",
+						value="",
+						height=150)
 
-# Process User Input Function
-def generate_bug_report(user_steps, expected_results):
-    """Processes the user's query, generates a response, and updates chat history."""
 
-    # Display the Assistant's message
-    with st.chat_message("assistant"):
-        response = process_user_input(user_steps, expected_results)
-        st.markdown(response)
+def get_test_env():
+	return st.text_area("Test environment details",
+						placeholder="",
+						value="- Environment: UAT\n- Platform: \n- Build version: \n- Browser: \n- Device (OS version): \n- Test account:",
+						height=170,
+						help="This helps in issue replication and investigation.")
+
+
+def show_caption_before():
+	caption = f"Streamline your QA process with ease!\n\n"
+	caption += f"👈🏻 Input your bug details, and let our AI generate clear and consistent bug reports for you."
+	st.write(caption)
+
+
+def show_caption_after():
+	caption = f"Report generated 🎉\n\n"
+	caption += f"*💡 If the result is not accurate, try to refine your input with more information.*"
+	st.write(caption)
+
+
+def show_placeholder(message="Loading..."):
+	custom_container = """
+			<div class="pre-generate-container">
+				<div class="pre-generate-icon">
+					<svg viewBox="0 0 24 24" aria-hidden="true" focusable="false" fill="currentColor" xmlns="http://www.w3.org/2000/svg" color="inherit" class="pre-generate-icon-svg">
+						<rect width="24" height="24" fill="none"></rect>
+						<path d="M20 9V7c0-1.1-.9-2-2-2h-3c0-1.66-1.34-3-3-3S9 3.34 9 5H6c-1.1 0-2 .9-2 2v2c-1.66 0-3 1.34-3 3s1.34 3 3 3v4c0 1.1.9 2 2 2h12c1.1 0 2-.9 2-2v-4c1.66 0 3-1.34 3-3s-1.34-3-3-3zm-2 10H6V7h12v12zm-9-6c-.83 0-1.5-.67-1.5-1.5S8.17 10 9 10s1.5.67 1.5 1.5S9.83 13 9 13zm7.5-1.5c0 .83-.67 1.5-1.5 1.5s-1.5-.67-1.5-1.5.67-1.5 1.5-1.5 1.5.67 1.5 1.5zM8 15h8v2H8v-2z"></path>
+					</svg>
+				</div>
+				<div class="pre-generate-message">{}</div>
+			</div>
+			""".format(message)
+
+	with st.container(height=160, border=True):
+		st.markdown(custom_container, unsafe_allow_html=True)
+
 
 try:
-    # Streamlit App Interface
-    st.set_page_config(
-        page_title="AI Bug Report Generator",
-        page_icon="🤖",
-        initial_sidebar_state="collapsed",
-        menu_items=None
-    )
-    st.title("AI Bug Report Generator")
+	# Check if required API key exists
+	check_api_key()
 
-    # Check if required API key exists
-    check_api_key()
+	# Streamlit App Interface
+	st.set_page_config(
+		page_title="IssueSnap",
+		page_icon="⚡️",
+		layout="centered",
+		initial_sidebar_state="expanded",
+		menu_items=None
+	)
 
-    with st.chat_message("assistant"):
-        st.markdown("Good day buddy! Tell me something about your bug 🐞")
+	# Custom CSS for centering the text and changing the font
+	st.markdown(
+		"""
+				<style>
+				.pre-generate-container {
+					height: 130px;
+					display: flex;
+					align-items: center;
+					justify-content: center;
+					gap: 0.5rem;
+				}
+				.pre-generate-icon {
+					display: flex;
+					width: 2rem;
+					height: 2rem;
+					flex-shrink: 0;
+					border-radius: 0.5rem;
+					-webkit-box-align: center;
+					align-items: center;
+					-webkit-box-pack: center;
+					justify-content: center;
+					background-color: rgb(255, 189, 69);
+				}
+				.pre-generate-icon-svg {
+					vertical-align: middle;
+					overflow: hidden;
+					color: inherit;
+					fill: currentcolor;
+					display: inline-flex;
+					-webkit-box-align: center;
+					align-items: center;
+					font-size: 1.25rem;
+					width: 1.25rem;
+					height: 1.25rem;
+					flex-shrink: 0;
+				}
+				.pre-generate-message {
+					font-family: Source Code Pro;
+					font-style: italic;
+					font-size: 16px;
+				}
+	
+				a:link , a:visited{
+					color: inherit;
+					background-color: transparent;
+					text-decoration: none;
+				}
 
-    # Get user input for steps
-    user_steps = get_user_steps()
+				a:hover,  a:active {
+					color: inherit;
+					background-color: transparent;
+					text-decoration: none;
+				}
+				</style>
+				""",
+		unsafe_allow_html=True
+	)
 
-    # Call the function when user provides steps
-    if user_steps:
-        # Display the User's steps
-        with st.chat_message("user"):
-            display_user_steps = "Steps to reproduce:\n\n" + user_steps
-            st.markdown(display_user_steps)
+	# Sidebar input view
+	with st.sidebar:
+		st.header("👇🏻 Tell me your bug")
+		user_steps = get_user_steps()
+		expected_results = get_expected_results()
+		test_env = get_test_env()
+		gen_btn = st.button("Generate", type="primary",
+							use_container_width=True)
+		st.caption("[Made with Passion © OURSKY](%s)" %
+				   "https://www.oursky.com")
 
-        # Get user input for steps
-        expected_results = get_expected_results()
-        if expected_results:
-            # Display the User's steps
-            with st.chat_message("user"):
-                display_expected_results = "Expected results:\n\n" + expected_results
-                st.markdown(display_expected_results)
+	# Main view
+	st.title("IssueSnap")
+	loading_holder = st.empty()
+	result_holder = st.empty()
+	if gen_btn:
+		# Show loading in container
+		with loading_holder.container():
+			result_holder.empty()
+			show_caption_before()
+			show_placeholder("Generating...")
+			response = process_user_input(
+				user_steps, expected_results, test_env)
+		# Show results in container
+		if response:
+			with result_holder.container():
+				loading_holder.empty()
+				show_caption_after()
+				with st.container(border=True):
+					st.write(response)
+				st.write("Happy Testing!🌟")
+	else:
+		with result_holder.container():
+			# Show default placeholder in container
+			show_caption_before()
+			show_placeholder("Your bug report will be generated here")
 
-            generate_bug_report(user_steps, expected_results)
 
-    footer = """
-                <style>
-                    .footer {
-                    position: relative;
-                    width: 100%;
-                    color: #888;
-                    text-align: right;
-                    font-size: 10px;
-                    font-weight: 400;
-                    }
-                </style>
-                <div class="footer">Made with Passion © Oursky Ltd.</div>
-        """
-    st.markdown(footer,unsafe_allow_html=True)
 except KeyError as e:
-    st.error(f"Missing environment variable: {e}")
-    logging.error(f"chatbot.py: Missing environment variable: {e}")
+	st.error(f"Missing environment variable: {e}")
+	logging.error(f"chatbot.py: Missing environment variable: {e}")
 except Exception as e:
-    st.error(f"Uh oh! We seem to be having a hiccup. Give it another shot soon?")
-    logging.error(f"chatbot.py: Exception: {e}")
+	st.error(f"Uh oh! We seem to be having a hiccup. Give it another shot soon?")
+	logging.error(f"chatbot.py: Exception: {e}")
